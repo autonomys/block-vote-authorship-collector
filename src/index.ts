87a95601d6034beda98dfe2e2b6777f8cb5dec1e
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2022-04-11 21:55:27 +0300

    Derive pledged space from solution range

--- src/index.ts
+++ src/index.ts
@@ -33,17 +33,27 @@ const types = {
     },
     SubPreDigest2022Jan02: {
         slot: 'u64',
-        solution: 'Solution2022Jan02'
+        solution: 'Solution2022Jan02',
     },
     Solution: {
         public_key: 'AccountId32',
-        reward_address: 'AccountId32'
+        reward_address: 'AccountId32',
     },
     SubPreDigest: {
         slot: 'u64',
-        solution: 'Solution'
+        solution: 'Solution',
     }
 
+}
+
+function solutionRangeToSpace(solutionRange: bigint): number {
+    const MAX_U64 = (2n ** 64n) - 1n;
+    const SLOT_PROBABILITY = [1n, 6n];
+    const PIECE_SIZE = 4096n;
+
+    return Number(
+        MAX_U64 * SLOT_PROBABILITY[0] / SLOT_PROBABILITY[1] / solutionRange * PIECE_SIZE
+    );
 }
 
 async function main(wsUrl: string, output: string) {
@@ -60,25 +70,31 @@ async function main(wsUrl: string, output: string) {
         : 'SubPreDigest';
 
     const file = await fs.open(output, 'w');
-    file.appendFile(`Block number;Slot;Author;Reward address\n`);
+    file.appendFile(
+        `Block number,Slot,Author,Reward address,Space according to consensus\n`,
+    );
 
     let nextBlockHash = await api.rpc.chain.getBlockHash();
     let processedBlocks = 0;
     while (true) {
         const header: Header = await api.rpc.chain.getHeader(nextBlockHash);
-        nextBlockHash = header.parentHash;
+
         const preRuntime: SubPreDigest = api.registry.createType(
             preDigestType,
             header.digest.logs
                 .find((digestItem) => digestItem.isPreRuntime)
                 ?.asPreRuntime![1]
-        )
+        );
+        const consensusSolutionRange = (await api.query.subspace.solutionRanges.at(nextBlockHash) as any).current.toBigInt();
 
         const blockNumber = header.number.toNumber();
         const slot = preRuntime.slot;
         const author = preRuntime.solution.public_key;
         const rewardAddress = preRuntime.solution.reward_address ?? author;
-        await file.appendFile(`${blockNumber};${slot};${author};${rewardAddress}\n`);
+        const consensusSpace = solutionRangeToSpace(consensusSolutionRange);
+        await file.appendFile(
+            `${blockNumber},${slot},${author},${rewardAddress},${consensusSpace}\n`,
+        );
 
         if (blockNumber === 0) {
             break;
@@ -89,6 +105,8 @@ async function main(wsUrl: string, output: string) {
         if (processedBlocks % 1000 === 0) {
             console.log(`Processed ${processedBlocks} blocks`);
         }
+
+        nextBlockHash = header.parentHash;
     }
 
     await file.close();
